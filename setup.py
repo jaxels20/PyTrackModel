ea8ab703e8fa516cc5c8896bb47922d6b937d32f
Jeppe Berg Axlesen <jaxels20@student.aau.dk>
2024-02-06 00:16:42 +0100

    changed wf

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyTrackModel",
+    name="torchversionist",
     version="0.0.1",
     author="Jeppe Berg Axelsen",
     author_email="xfm82gtn@gmail.com",
